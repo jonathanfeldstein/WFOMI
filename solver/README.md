9829ee0cdca18b39572036f20c88a1828811b776
mbkorecki <mbkorecki@gmail.com>
2020-04-09 20:43:41 +0200

    computes both the partition function and the query function

--- solver/README.md
+++ solver/README.md
@@ -1,5 +1,9 @@
-run with python3 main.py circuit_filename weight_filename
+run with python3 main.py circuit1_filename circuit2_filename weight_filename
+
+where circuit 1 represents the partition circuit so the one on the entire theory
+and circuit 2 represents the query circuit
+
 
 eg.
-python3 main.py test_input/circuit_diab.txt test_input/weights_simple.txt
+python3 main.py test_input/circuit_diab.txt test_input/circuit_diab_query.txt test_input/weights_simple.txt
 
--- solver/main.py
+++ solver/main.py
@@ -4,18 +4,24 @@ import time
 import sys
 
 def main():
-    circuitFile = sys.argv[1]
-    weightFile = sys.argv[2]
+    partitionFile = sys.argv[1]
+    queryFile = sys.argv[2]
+    weightFile = sys.argv[3]
     
     parser = Parser()
     weights, domains = parser.parseWeights(weightFile)
-    root, nodes = parser.parseCircuit(circuitFile, weights, domains)
+    partitionRoot, partitionNodes = parser.parseCircuit(partitionFile, weights, domains)
+    queryRoot, queryNodes = parser.parseCircuit(queryFile, weights, domains)
 
     start = time.time()
-    result = nodes[root].compute()
+    partitionF = partitionNodes[partitionRoot].compute()
+    queryF = queryNodes[queryRoot].compute()
+    queryProb = queryF / partitionF
     end = time.time()
     
-    print(result)
+    print("partition function =", partitionF)
+    print("the query =", queryF)
+    print("P(query) =", queryProb)
     print("time to compute:", end - start)
 
 
