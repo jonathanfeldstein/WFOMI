13ce2901851cb3ee2ad68a572c2865278570efa6
Jonathan Feldstein <jonathan.feldstein@ed.ac.uk>
2020-05-21 18:49:28 +0200

    Fixed to new algorithm

--- solver/term_efficient.py
+++ solver/term_efficient.py
@@ -14,11 +14,6 @@ class Term(object):
             self.wfs = [1]
         else:
             self.wfs = wfs
-        # # args are the variables of the weight functions
-        # if args is None:
-        #     self.args = []
-        # else:
-        #     self.args = args
         # bounds are integral bounds
         if bounds is None:
             self.bounds = [{}]
@@ -43,20 +38,20 @@ class Term(object):
         bounds = []
         for bounds1 in self.bounds:
             for bounds2 in other.bounds:
-                new_bounds = bounds1
+                new_bound = bounds1.copy()
                 bounds1_set = set(bounds1)
                 bounds2_set = set(bounds2)
                 for variable in bounds1_set & bounds2_set:
-                    new_bounds[variable] = [max(bounds1.get(variable)[0], bounds2.get(variable)[0]), min(bounds1.get(variable)[1], bounds2.get(variable)[1])]
+                    new_bound[variable] = [max(bounds1.get(variable)[0], bounds2.get(variable)[0]), min(bounds1.get(variable)[1], bounds2.get(variable)[1])]
                 for variable in bounds2_set-bounds1_set:
-                    new_bounds[variable] = bounds2.get(variable)
-                bounds.append(new_bounds)
+                    new_bound[variable] = bounds2.get(variable)
+                bounds.append(new_bound)
         return Term(wfs, bounds, cst)
 
     def __str__(self):
         return "wfs: " + str(self.wfs) + " bounds: " + str(self.bounds) + "cst: " + str(self.cst)
 
     def integrate(self):
-        integrated = {wf: wf.integrate(*wf.free_symbols) for wf in set(self.wfs)}
-        integral = [symbolic_to_numeric(integrated[self.wfs[i]], self.bounds[i])*self.cst[i] for i in range(len(self.wfs))]
+        integrated = {wf: wf.integrate(*wf.free_symbols) if hasattr(wf, 'free_symbols') else wf for wf in set(self.wfs)}
+        integral = [symbolic_to_numeric(integrated[self.wfs[i]], self.bounds[i])*self.cst[i] if hasattr(self.wfs[i], 'free_symbols') else self.wfs[i]*self.cst[i] for i in range(len(self.wfs))]
         return Term([1], [{}], [sum(integral)])
